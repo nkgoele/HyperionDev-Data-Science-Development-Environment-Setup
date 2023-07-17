8a3c11221de00debd610cb5628e836856ea6f15f
nkgoele <139330821+nkgoele@users.noreply.github.com>
2023-07-17 22:08:50 +0200

    numbers.py

---
+++ numbers.py
@@ -0,0 +1,36 @@
+##Request the user to enter three different integers
+##Assign the variables _a, _b, _c to the three integers
+##print out variable_a
+##print out variable_b
+##print out variable_c
+##Add variables a + b + c
+##Assign sum_1 to the added variables
+##print (sum1)
+
+##Subtract var_a - var_b 
+##Assign diff_1 to (var_a - var_b)
+##print(diff1)
+
+##Multiply var_c by var_a 
+##Assign multip1  to (var_c * var_a)
+##print(multip1)
+
+##Divide (sum1) by var_c i.e. (sum1 / c) 
+##Assign div1  to (sum1 / c) 
+##print(div1)
+
+## Entering any three different integers:
+a = int(input('Enter 1st number: '))
+print(a)
+b = int(input('Enter 2nd number: '))
+print(b)
+c = int(input('Enter 3rd number: '))
+print(c)
+sum1 = a + b + c
+print(sum1)
+dif1 = a - b
+print(dif1)
+mult1 = c * a
+print(mult1)
+div1 = int((sum1 / c))
+print(div1)
