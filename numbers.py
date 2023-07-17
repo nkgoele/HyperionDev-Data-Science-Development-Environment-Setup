a85ca574954db070964d3598fe112fbf7de6717f
nkgoele <139330821+nkgoele@users.noreply.github.com>
2023-07-17 16:50:31 +0200

    numbers.py

---
+++ numbers.py
@@ -0,0 +1,6 @@
+##Ask the user to enter three different integers
+##Then print out:
+##The sum of all the numbers
+##○ The first number minus the second number
+##○ The third number multiplied by the first number
+##○ The sum of all three numbers divided by the third number
